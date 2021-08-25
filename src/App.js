87735c4507f14103ee9f4f4af845df43288b5be9
Liliana Blattner <lilian14torres@gmail.com>
2021-08-25 13:20:38 -0700

    dded search engine and styled css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+import lupa from "./images/lupa.jpeg";
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        hello
+        <img src={lupa} className="App-lupa" alt="lupa" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Liliana Blattner</small>
+      </footer>
     </div>
+  </div>
   );
 }
 
-export default App;
+
